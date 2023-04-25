58d1138e3767e22b7bb4a114f06daff3024abd4d
devziad2021 <devziad2021@gmail.com>
2023-04-26 04:09:19 +0700

    fixedq

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import AboutPage from "./pages/AboutPage";
 import DetailSkill from "./pages/DetailSkill";
 import Introduction from "./pages/Introduction";
 import SkillPage from "./pages/SkillPage";
-import { NotFound } from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Introduction />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/skill" element={<SkillPage />} />
-      <Route path="/skill/:item" element={<DetailSkill />} />
+      <Route path="\" element={<Introduction />} />
+      <Route path="\about" element={<AboutPage />} />
+      <Route path="\skill" element={<SkillPage />} />
+      <Route path="\skill\:item" element={<DetailSkill />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
